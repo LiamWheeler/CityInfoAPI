bba6bde68bdb0cc6868de61798168a1c22fa0a8f
liam wheeler <liam_wheeler@hotmail.com>
2019-07-01 10:49:22 +0100

    Add project files.

---
+++ src/CityInfo.API/Models/CityDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Models
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
