4c8de5af674bbaeb4d80a6e3dbe07e1355887fa0
liam wheeler <liam_wheeler@hotmail.com>
2019-07-01 11:27:39 +0100

    relocated CitiesController class

--- src/CityInfo.API/Models/CityDto.cs
+++ src/CityInfo.API/Models/CityDto.cs
@@ -8,6 +8,8 @@ namespace CityInfo.API.Models
     public class CityDto
     {
         public int Id { get; set; }
-        public String Name { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int NumberOfPointsOfInterest { get; set; }
     }
 }
