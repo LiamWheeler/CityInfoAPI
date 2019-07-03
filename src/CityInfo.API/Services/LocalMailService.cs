528bb509f638b6b04da77d6e07433f34605c2e5a
liam wheeler <liam_wheeler@hotmail.com>
2019-07-03 11:47:35 +0100

    Added Mail service code to write to debug console

--- src/CityInfo.API/Services/LocalMailService.cs
+++ src/CityInfo.API/Services/LocalMailService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace CityInfo.API.Services
 {
     public class LocalMailService
     {
+
+        private string _mailTo = "admin@mycompany.com";
+        private string _mailFrom = "noreply@mycompany.com";
+
+        public void Send(string subject, string message)
+        {
+            //send mail - output to debug window
+            Debug.WriteLine($"Mail From {_mailFrom} to {_mailTo}, with LocalMailServices");
+            Debug.WriteLine($"Subject: {subject}");
+            Debug.WriteLine($"Message: {message}");
+
+        }
     }
 }
