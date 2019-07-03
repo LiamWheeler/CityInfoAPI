72a42372002cb64312df245aab1d6fc31d8b5f72
liam wheeler <liam_wheeler@hotmail.com>
2019-07-03 14:29:55 +0100

    Updated to use IMailServices

--- src/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ src/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -14,10 +14,10 @@ namespace CityInfo.API.Controllers
     public class PointsOfInterestController : Controller
     {
         private ILogger<PointsOfInterestController> _logger;
-        private LocalMailService _mailService;
+        private IMailServices _mailService;
 
         public PointsOfInterestController(ILogger<PointsOfInterestController> logger, 
-            LocalMailService mailService)
+            IMailServices mailService)
         {
             _logger = logger;
             _mailService = mailService;
