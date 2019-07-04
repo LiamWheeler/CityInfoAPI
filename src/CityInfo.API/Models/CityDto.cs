acc105b7a0d016c82ad63bb19bdfc5c27cd9b1f7
liam wheeler <liam_wheeler@hotmail.com>
2019-07-04 10:01:35 +0100

    Added [Key]

--- src/CityInfo.API/Models/CityDto.cs
+++ src/CityInfo.API/Models/CityDto.cs
@@ -7,6 +7,7 @@ namespace CityInfo.API.Models
 {
     public class CityDto
     {
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
